c8dedc733ba58575aa5feb192363d1c948944374
suerain <surendralamichhane07@gmail.com>
2018-05-28 12:33:41 -0500

    firstcommit

---
+++ src/main/java/com/amazonws/AmazonwsApplication.java
@@ -0,0 +1,12 @@
+package com.amazonws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonwsApplication.class, args);
+	}
+}
